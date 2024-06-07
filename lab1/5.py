201c71ef8bcec37ac502bffe1ba1150d157249d5
Ilyas700RK <123076961+Ilyas700RK@users.noreply.github.com>
2024-06-07 11:55:10 +0500

    Add files via upload

---
+++ lab1/5.py
@@ -0,0 +1,61 @@
+carname = "Volvo"
+
+
+x = 50
+
+
+x = 5
+y = 10
+print(x + y)
+
+
+x = 5
+y = 10
+z = x + y
+print(z)
+
+
+x ,y, z = "Orange", "Banana", "Cherry"
+
+
+x = y = z = "Orange"
+
+x = 5
+print(type(x))
+int
+
+
+x = "Hello World"
+print(type(x))
+str
+
+
+
+x = 20.5
+print(type(x))
+float
+
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+
+x = True
+print(type(x))
+bool 
+
+
+
+
+
+
